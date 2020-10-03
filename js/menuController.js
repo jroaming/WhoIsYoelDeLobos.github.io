163fe2851f765123842aa0306ccebab06b4fd3cf
Joel López <47560618+jroaming@users.noreply.github.com>
2020-10-03 03:42:42 +0200

    Cover finished - Profile started

---
+++ js/menuController.js
@@ -0,0 +1,10 @@
+function switchTo(code) {
+    //$('loading-screen').style.display == 'none';
+    $('#main-cover')[0].style.display = 'none';
+    $('#profile-page')[0].style.display = 'none';
+    $('#projects-page')[0].style.display = 'none';
+    $('#tools-page')[0].style.display = 'none';
+    $('#contact-page')[0].style.display = 'none';
+
+    $('#'+code)[0].style.display = 'block';
+}
--- js/mouseGlow.js
+++ js/mouseGlow.js
@@ -1,11 +1,39 @@
-// Mouse-around glow FX
+var audio1 = document.getElementsByTagName("audio")[0];
+var audio2 = document.getElementsByTagName("audio")[1];
+var audio3 = document.getElementsByTagName("audio")[2];
+var audio4 = document.getElementsByTagName("audio")[3];
 
 $(document).mousemove(function(event) {
-    windowWidth = $(window).width();
-    windowHeight = $(window).height();
-    
-    mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
-    mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-    
-    $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #04003f, #000000)');
+
+  // Mouse-around background glow FX
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
+  
+  mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+  
+  $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #04003f, #000000)');
+
+
+  // Menu FX
+  $("#div-main-menu-item1").hover(function() {
+    audio1.play();
+  }, function() {
+    audio1.currentTime = 0;
   });
+  $("#div-main-menu-item2").hover(function() {
+    audio2.play();
+  }, function() {
+    audio2.currentTime = 0;
+  });
+  $("#div-main-menu-item3").hover(function() {
+    audio3.play();
+  }, function() {
+    audio3.currentTime = 0;
+  });
+  $("#div-main-menu-item4").hover(function() {
+    audio4.play();
+  }, function() {
+    audio4.currentTime = 0;
+  });
+});
